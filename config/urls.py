1bbb7565327cd8501ea28f0b8e6cc772ec9cb3aa
deus99999 <rudenkooleksei@gmail.com>
2026-04-12 21:10:30 +0300

    add render avatar img

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from comments.views import CommentViewSet
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'comments', CommentViewSet)
@@ -10,3 +12,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/src/components/CommentItem.jsx
+++ frontend/src/components/CommentItem.jsx
@@ -3,11 +3,24 @@ import '../App.css';
 
 // Only one comment
 const CommentItem = ({ comment }) => {
+  const defaultAvatar = 'https://cdn-icons-png.flaticon.com/512/149/149071.png';
+
   return (
     <div className="comment-wrapper">
       <div className="comment-container">
         <div className="comment-header">
-          <div className="avatar"></div>
+          <div className="avatar">
+          <img 
+              src={comment.file ? comment.file : defaultAvatar} 
+              alt="avatar" 
+              style={{
+                width: '100%',
+                height: '100%',
+                borderRadius: '50%',
+                objectFit: 'cover' 
+              }} 
+            />
+          </div>
           <span className="username">
                     {comment.user_name}
           </span>
@@ -18,11 +31,11 @@ const CommentItem = ({ comment }) => {
           {comment.text}
         </div>
 
-        {comment.file && (
+        {/* {comment.file && (
           <div className="comment-file">
             <a href={comment.file} target="_blank" rel="noreferrer">📎 Файл</a>
           </div>
-        )}
+        )} */}
       </div>
 
       {comment.replies && comment.replies.length > 0 && (
